4fd259b52bf17b8a1c7c8839068fc2d7ce283efd
adelguit <adeelguitoun@gmail.com>
2023-02-21 20:22:03 +0100

    Builde the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import Greeting from './components/Greeting';
 
-const App = () => {};
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </Provider>
+  );
+};
 
 export default App;
